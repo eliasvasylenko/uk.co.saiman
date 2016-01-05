3d794678ed53317e67a27e0f1e2a352413eef087
Elias N Vasylenko <eliasvasylenko@gmail.com>
2016-01-05 10:25:52 +0000

    Fixed build issues
    - added license texts
    - worked around 'multiple references with same name' osgi ds bug
    - added some missing run/build dependencies

--- uk.co.saiman.msapex.acquisition/src/uk/co/saiman/msapex/acquisition/AcquisitionModulesSupplier.java
+++ uk.co.saiman.msapex.acquisition/src/uk/co/saiman/msapex/acquisition/AcquisitionModulesSupplier.java
@@ -1,24 +1,29 @@
+/*
+ * Copyright (C) 2016 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
+ *
+ * This file is part of uk.co.saiman.msapex.acquisition.
+ *
+ * uk.co.saiman.msapex.acquisition is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uk.co.saiman.msapex.acquisition is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.co.saiman.msapex.acquisition;
 
 import org.eclipse.e4.core.di.suppliers.ExtendedObjectSupplier;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 
 import uk.co.saiman.eclipse.ObservableListSupplier;
 import uk.co.saiman.instrument.acquisition.AcquisitionModule;
 
 @Component(service = ExtendedObjectSupplier.class, property = "dependency.injection.annotation=uk.co.saiman.msapex.acquisition.AcquisitionModules")
 public class AcquisitionModulesSupplier extends ObservableListSupplier<AcquisitionModule> {
-	@Override
-	@Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
-	public void addItem(AcquisitionModule item) {
-		super.addItem(item);
-	}
-
-	@Override
-	public void removeItem(AcquisitionModule item) {
-		super.removeItem(item);
-	}
 }
