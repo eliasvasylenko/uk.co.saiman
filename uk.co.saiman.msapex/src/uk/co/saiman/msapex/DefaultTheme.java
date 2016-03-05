ca8bf3a0c120bbbe78dd610d851dcfb4d613eaa8
Elias N Vasylenko <eliasvasylenko@gmail.com>
2016-03-05 23:57:11 +0000

    -

--- uk.co.saiman.msapex/src/uk/co/saiman/msapex/DefaultTheme.java
+++ uk.co.saiman.msapex/src/uk/co/saiman/msapex/DefaultTheme.java
@@ -18,13 +18,9 @@
  */
 package uk.co.saiman.msapex;
 
-import org.eclipse.fx.ui.services.theme.Stylesheet;
 import org.eclipse.fx.ui.services.theme.Theme;
 import org.eclipse.fx.ui.theme.AbstractTheme;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
 
 /**
  * Default css theme for JavaFX components.
@@ -38,16 +34,5 @@ public class DefaultTheme extends AbstractTheme {
 	 */
 	public DefaultTheme() {
 		super("theme.default", "Default theme", DefaultTheme.class.getClassLoader().getResource("css/default.css"));
-	}
-
-	@Override
-	@Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
-	public void registerStylesheet(Stylesheet stylesheet) {
-		super.registerStylesheet(stylesheet);
-	}
-
-	@Override
-	public void unregisterStylesheet(Stylesheet stylesheet) {
-		super.unregisterStylesheet(stylesheet);
 	}
 }
