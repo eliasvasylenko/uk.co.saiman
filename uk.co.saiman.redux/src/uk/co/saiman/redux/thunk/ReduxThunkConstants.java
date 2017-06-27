cee83023d2d168b683a2cd4a4213b5fcaefb878a
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-06-27 10:04:26 +0100

    added redux thunk webresource

---
+++ uk.co.saiman.redux/src/uk/co/saiman/redux/thunk/ReduxThunkConstants.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
+ *          ______         ___      ___________
+ *       ,'========\     ,'===\    /========== \
+ *      /== \___/== \  ,'==.== \   \__/== \___\/
+ *     /==_/____\__\/,'==__|== |     /==  /
+ *     \========`. ,'========= |    /==  /
+ *   ___`-___)== ,'== \____|== |   /==  /
+ *  /== \__.-==,'==  ,'    |== '__/==  /_
+ *  \======== /==  ,'      |== ========= \
+ *   \_____\.-\__\/        \__\\________\/
+ *
+ * This file is part of uk.co.saiman.redux.webresource.
+ *
+ * uk.co.saiman.redux.webresource is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uk.co.saiman.redux.webresource is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.co.saiman.redux.thunk;
+
+import static uk.co.saiman.redux.thunk.ReduxThunkConstants.REDUX_THUNK_WEB_RESOURCE_NAME;
+import static uk.co.saiman.redux.thunk.ReduxThunkConstants.REDUX_THUNK_WEB_RESOURCE_VERSION;
+
+import aQute.bnd.annotation.headers.ProvideCapability;
+import osgi.enroute.namespace.WebResourceNamespace;
+
+@ProvideCapability(
+		ns = WebResourceNamespace.NS,
+		version = REDUX_THUNK_WEB_RESOURCE_VERSION,
+		value = ("root=/META-INF/resources/webjars/redux-thunk/"
+				+ REDUX_THUNK_WEB_RESOURCE_VERSION) + ";"
+				+ (WebResourceNamespace.NS + "=" + REDUX_THUNK_WEB_RESOURCE_NAME))
+public interface ReduxThunkConstants {
+	final String REDUX_THUNK_WEB_RESOURCE_NAME = "/redux-thunk/redux-thunk";
+	final String REDUX_THUNK_WEB_RESOURCE_VERSION = "2.1.0";
+}
