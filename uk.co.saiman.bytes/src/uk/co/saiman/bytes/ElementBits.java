a60689aad088eb6f6c35cb36a816e6e31a9c4e7e
Elias N Vasylenko <eliasvasylenko@gmail.com>
2018-02-28 10:06:34 +0000

    fixed some nonsense licence problems, plugin is a bit unreliable...

--- uk.co.saiman.bytes/src/uk/co/saiman/bytes/ElementBits.java
+++ uk.co.saiman.bytes/src/uk/co/saiman/bytes/ElementBits.java
@@ -10,14 +10,14 @@
  *  \======== /==  ,'      |== ========= \
  *   \_____\.-\__\/        \__\\________\/
  *
- * This file is part of uk.co.saiman.comms.
+ * This file is part of uk.co.saiman.bytes.
  *
- * uk.co.saiman.comms is free software: you can redistribute it and/or modify
+ * uk.co.saiman.bytes is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uk.co.saiman.comms is distributed in the hope that it will be useful,
+ * uk.co.saiman.bytes is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
