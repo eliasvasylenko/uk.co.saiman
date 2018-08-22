56476d92863fd109ecb0a1d3af0fdfaa06bf1c10
Elias N Vasylenko <eliasvasylenko@gmail.com>
2018-08-22 12:47:01 +0100

    updated root .gitignore and added missing cache

---
+++ uk.co.saiman.webmodules.commonjs/src/uk/co/saiman/webmodule/commonjs/cache/Cache.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2018 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
+ *          ______         ___      ___________
+ *       ,'========\     ,'===\    /========== \
+ *      /== \___/== \  ,'==.== \   \__/== \___\/
+ *     /==_/____\__\/,'==__|== |     /==  /
+ *     \========`. ,'========= |    /==  /
+ *   ___`-___)== ,'== \____|== |   /==  /
+ *  /== \__.-==,'==  ,'    |== '__/==  /_
+ *  \======== /==  ,'      |== ========= \
+ *   \_____\.-\__\/        \__\\________\/
+ *
+ * This file is part of uk.co.saiman.webmodules.commonjs.
+ *
+ * uk.co.saiman.webmodules.commonjs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uk.co.saiman.webmodules.commonjs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.co.saiman.webmodule.commonjs.cache;
+
+import static java.nio.file.Files.walk;
+import static java.util.Comparator.reverseOrder;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import uk.co.saiman.function.ThrowingConsumer;
+import uk.co.saiman.function.ThrowingSupplier;
+import uk.co.saiman.webmodule.commonjs.RegistryResolutionException;
+
+/**
+ * A shared local cache for resources derived from CommonJS registry packages.
+ * 
+ * @author Elias N Vasylenko
+ *
+ */
+public class Cache {
+  private static final int BUFFER_SIZE = 1024;
+  private static final int MAXIMUM_DOWNLOAD_ATTEMPTS = 3;
+
+  private final Path cacheRoot;
+
+  public Cache(Path cacheRoot) {
+    this.cacheRoot = cacheRoot;
+  }
+
+  public Path getCacheRoot() {
+    return cacheRoot;
+  }
+
+  public Path fetchResource(String resourceName, ThrowingConsumer<CacheEntry, IOException> prepare)
+      throws IOException {
+    return fetchResource(resourceName, prepare, Retention.STRONG);
+  }
+
+  public <E extends Exception> Path fetchResource(
+      String resourceName,
+      ThrowingConsumer<CacheEntry, E> prepare,
+      Retention retention) throws E {
+    Path destination = getCacheRoot().resolve(resourceName);
+
+    if (Files.exists(destination)) {
+      if (retention == Retention.STRONG) {
+        return destination;
+      } else {
+        try {
+          clearCache(destination);
+        } catch (IOException e) {
+          throw new RegistryResolutionException("Unable to clear destination " + destination, e);
+        }
+      }
+    }
+
+    CacheEntry entry = new CacheEntry(destination);
+    int attempt = MAXIMUM_DOWNLOAD_ATTEMPTS;
+    do {
+      try {
+        prepare.accept(entry);
+        attempt = 0;
+      } catch (Exception e) {
+        if (--attempt == 0) {
+          if (Files.exists(destination)) {
+            try {
+              clearCache(destination);
+            } catch (IOException h) {
+              e.addSuppressed(h);
+            }
+          }
+          throw e;
+        }
+      }
+    } while (attempt > 0);
+
+    entry.complete();
+
+    return destination;
+  }
+
+  private void clearCache(Path destination) throws IOException {
+    walk(destination).sorted(reverseOrder()).map(Path::toFile).forEach(File::delete);
+  }
+
+  public static <T extends Throwable> byte[] getBytes(ThrowingSupplier<InputStream, T> input)
+      throws T, IOException {
+    try (InputStream stream = input.get()) {
+      return getBytes(stream);
+    }
+  }
+
+  public static byte[] getBytes(InputStream input) throws IOException {
+    input = new BufferedInputStream(input);
+
+    try (ByteArrayOutputStream buffered = new ByteArrayOutputStream()) {
+      byte[] readBuffer = new byte[BUFFER_SIZE];
+      int len = 0;
+      while ((len = input.read(readBuffer)) != -1) {
+        buffered.write(readBuffer, 0, len);
+      }
+
+      return buffered.toByteArray();
+    }
+  }
+}
---
+++ uk.co.saiman.webmodules.commonjs/src/uk/co/saiman/webmodule/commonjs/cache/CacheEntry.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2018 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
+ *          ______         ___      ___________
+ *       ,'========\     ,'===\    /========== \
+ *      /== \___/== \  ,'==.== \   \__/== \___\/
+ *     /==_/____\__\/,'==__|== |     /==  /
+ *     \========`. ,'========= |    /==  /
+ *   ___`-___)== ,'== \____|== |   /==  /
+ *  /== \__.-==,'==  ,'    |== '__/==  /_
+ *  \======== /==  ,'      |== ========= \
+ *   \_____\.-\__\/        \__\\________\/
+ *
+ * This file is part of uk.co.saiman.webmodules.commonjs.
+ *
+ * uk.co.saiman.webmodules.commonjs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uk.co.saiman.webmodules.commonjs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.co.saiman.webmodule.commonjs.cache;
+
+import static java.nio.file.Files.createDirectories;
+import static java.nio.file.Files.newOutputStream;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import uk.co.saiman.webmodule.commonjs.RegistryResolutionException;
+
+public class CacheEntry {
+  private final Path location;
+
+  public CacheEntry(Path location) {
+    this.location = location;
+  }
+
+  public Path getLocation() {
+    return location;
+  }
+
+  public Path writeBytes(byte[] bytes) {
+    return writeBytesImpl(getLocation(), bytes);
+  }
+
+  public Path writeBytes(String resourceName, byte[] bytes) {
+    return writeBytesImpl(getLocation().resolve(resourceName), bytes);
+  }
+
+  public Path writeBytes(Path destination, byte[] bytes) {
+    return writeBytesImpl(getLocation().resolve(destination), bytes);
+  }
+
+  private Path writeBytesImpl(Path destination, byte[] bytes) {
+    try {
+      createDirectories(destination.getParent());
+
+      try (BufferedOutputStream buffered = new BufferedOutputStream(newOutputStream(destination))) {
+        buffered.write(bytes);
+        buffered.flush();
+      }
+
+      return destination;
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RegistryResolutionException(
+          "Failed to write bytes to cache directory " + destination,
+          e);
+    }
+  }
+
+  /**
+   * In case two instances are trying to write to the same location, i.e. one or
+   * more registry instances handling multiple resource with the same sha1, we
+   * must guard by writing to a unique, safe location then moving to the cache
+   * location when done. If we find something is there by the time we're finished
+   * we can just discard our work and use the existing entry.
+   */
+  void complete() {
+    // TODO
+  }
+}
