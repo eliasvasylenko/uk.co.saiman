4acfe80f8fa8d03ca0cc369734f176d64fad2fa9
Elias N Vasylenko <eliasvasylenko@gmail.com>
2018-11-02 12:05:35 +0000

    license texts

--- uk.co.saiman.data/src/uk/co/saiman/data/function/processing/Convolution.java
+++ uk.co.saiman.data/src/uk/co/saiman/data/function/processing/Convolution.java
@@ -10,14 +10,14 @@
  *  \======== /==  ,'      |== ========= \
  *   \_____\.-\__\/        \__\\________\/
  *
- * This file is part of uk.co.saiman.experiment.processing.
+ * This file is part of uk.co.saiman.data.
  *
- * uk.co.saiman.experiment.processing is free software: you can redistribute it and/or modify
+ * uk.co.saiman.data is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uk.co.saiman.experiment.processing is distributed in the hope that it will be useful,
+ * uk.co.saiman.data is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
