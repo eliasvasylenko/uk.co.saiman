2ea3c49a117f0a3dc2639f87ba4feb4c2b8b31e7
Elias N Vasylenko <eliasvasylenko@gmail.com>
2019-04-24 11:03:10 +0100

    missing from previous commit for copley webconsole...

--- uk.co.saiman.copley.webconsole/webmodule/index.js
+++ uk.co.saiman.copley.webconsole/webmodule/index.js
@@ -1,25 +1,3 @@
-import { html, render } from 'lighterhtml'
+import { init } from './reactor.js'
 
-import { ConsoleComponent } from '@saiman/webconsole'
-
-import { requestInfo } from './actions.js'
-import dispatch from './model.js'
-import state from './state.js'
-import CopleyInformationController from './view.js'
-import CopleyTableController from './variables.js'
-
-requestInfo()
-
-render(
-  html`
-    <div id="copleyApp">
-      ${CopleyInformation({
-		name: model.name,
-        connection: model.connection,
-        bundle: model.bundle,
-        locale:model.locale
-      })}
-    </div>
-  `,
-  document.getElementById( 'content' )
-)
+init()
