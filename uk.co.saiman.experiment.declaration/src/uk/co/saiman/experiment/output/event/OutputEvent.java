ee8c094ff37226c9ab65702670c9100014bbcad3
Elias N Vasylenko <elias@vasylenko.uk>
2020-09-17 15:38:19 +0100

    updated license texts

--- uk.co.saiman.experiment.declaration/src/uk/co/saiman/experiment/output/event/OutputEvent.java
+++ uk.co.saiman.experiment.declaration/src/uk/co/saiman/experiment/output/event/OutputEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
+ * Copyright (C) 2020 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
  *          ______         ___      ___________
  *       ,'========\     ,'===\    /========== \
  *      /== \___/== \  ,'==.== \   \__/== \___\/
@@ -10,14 +10,14 @@
  *  \======== /==  ,'      |== ========= \
  *   \_____\.-\__\/        \__\\________\/
  *
- * This file is part of uk.co.saiman.experiment.procedure.
+ * This file is part of uk.co.saiman.experiment.declaration.
  *
- * uk.co.saiman.experiment.procedure is free software: you can redistribute it and/or modify
+ * uk.co.saiman.experiment.declaration is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uk.co.saiman.experiment.procedure is distributed in the hope that it will be useful,
+ * uk.co.saiman.experiment.declaration is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
